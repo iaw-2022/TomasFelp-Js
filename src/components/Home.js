df15772e7c7e82cfb573c765abde5ce6bd9a0461
Tomas Felipe <tomasfelipe911@gmail.com>
2022-05-27 12:07:57 -0300

    Se definio un componente home para completar en el futuro

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import logo from '../logo.svg';
+//import 'src\App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Under Music App
+        </p>
+        <a
+          className="App-link"
+        >
+          Coming soon
+        </a>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
