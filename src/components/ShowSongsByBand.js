563a924173861a32d56f3453d7752545ef7d7825
Tomas Felipe <tomasfelipe911@gmail.com>
2022-05-27 21:25:20 -0300

    Primer implementacion de componente para mostrar las canciones de una banda

---
+++ src/components/ShowSongsByBand.js
@@ -0,0 +1,72 @@
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router'
+
+
+import { Link } from 'react-router-dom'
+
+const endpoint= 'https://indie-music.herokuapp.com/api'
+
+const ShowSongsByBand = () => {
+    const [ songs, setSongs] = useState([])
+    const [ search, setSearch] = useState("")
+    
+    let { band } = useParams();
+
+    useEffect(() => {
+        getBands()
+    }, [])
+
+    const searcher = (e) => {
+        setSearch(e.target.value)
+    }
+
+    const results = !search ? songs : songs.filter((dato)=> dato.name.toLowerCase().includes(search.toLocaleLowerCase()))
+
+    const getBands = async () => {
+        const response = await axios.get(`${endpoint}/band/songs/${band}`)
+        setSongs(response.data)
+    }
+
+    return (
+        <div className="container-md">
+            <div>
+                <h2 className="text-center">
+                    Songs
+                </h2>
+
+                <input value={search} onChange={searcher} type="text" placeholder="Search" className="form-control mt-5 shadow-lg bg-dark text-white"/>
+            </div>
+           
+           <div>
+                <table className='table table-dark table-striped mt-5 shadow-lg'>
+                    <thead>
+                        <th class="bg-black">name</th>
+                        <th class="bg-black">album</th>
+                        <th class="bg-black">release date</th>
+                    </thead>
+                    <tbody>
+                        {results.map((genre) => (
+                            <tr key={genre.id}>
+                                <td className="border border-0 rounded-end rounded-3">
+                                    {genre.name}
+                                </td>
+                                <td className="border border-0 rounded-end rounded-3">
+                                    {genre.album}
+                                </td>
+                                <td className="border border-0 rounded-end rounded-3">
+                                    {genre.release_date}
+                                </td>
+                                <td className="border border-0 rounded-end rounded-3">
+                                    {genre.year}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
+
+export default ShowSongsByBand
