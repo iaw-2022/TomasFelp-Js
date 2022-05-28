09e042743a4582bd604ca23e5c78c8127f132c30
Tomas Felipe <tomasfelipe911@gmail.com>
2022-05-28 19:50:59 -0300

    Se agrego backround y ruta para una cancion

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import './Background.css';
 import './App.css';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -6,20 +6,25 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ShowGenres from './components/ShowGenres';
 import ShowBandsByGenre from './components/ShowBandsByGenre';
 import ShowSongsByBand from './components/ShowSongsByBand';
+import Song from './components/Song';
 import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-   
 
+      <div class="bg"></div>
+      <div class="bg bg2"></div>
+      <div class="bg bg3"></div>
+   
         <BrowserRouter>
           <Routes> 
              <Route path="/" element={<Home/>} />
              <Route path="/genres" element={<ShowGenres/>} />
              <Route path="/bands/:genre" element={<ShowBandsByGenre/>} />
              <Route path="/songs/:band" element={<ShowSongsByBand/>} />
+             <Route path="/song/:band" element={<Song/>} />
           </Routes> 
         </BrowserRouter>
 
